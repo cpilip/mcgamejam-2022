04c1d5f36b831e7616cb60b26c274479a58a1801
cpilip <pilipchristina@gmail.com>
2022-03-12 15:51:25 -0500

    Red Puzzle implemented

---
+++ Assets/Scripts/Interactable/InteractableNote.cs
@@ -0,0 +1,22 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class InteractableNote : Interactable
+{
+    [SerializeField] private bool m_Read = false;
+    public override void InteractWith()
+    {
+        if (!m_Read)
+        {
+            m_Read = true;
+            DisablePing();
+        }
+       
+    }
+
+    public void ResetState()
+    {
+       
+    }
+}
